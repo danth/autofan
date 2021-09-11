a9b4761b5a24d161bd6ae65418074015d3a92d00
Daniel Thwaites <danthwaites30@btinternet.com>
2021-09-11 16:50:12 +0100

    Fetch temperature from API :sparkles: :tada:

---
+++ Autofan.hs
@@ -0,0 +1,79 @@
+import Data.List ( intercalate )
+import Data.Maybe ( mapMaybe )
+import Text.XML.Light ( Element, QName(QName), findAttr, findElement, findElements, parseXML, onlyElems )
+import Network.HTTP ( Request, simpleHTTP, getRequest, getResponseBody )
+import System.Environment ( getArgs )
+import System.Exit ( ExitCode(ExitFailure), exitWith )
+import System.IO ( hPutStrLn, stderr )
+
+-- General utilities
+
+average :: (Num a, Fractional a) => [a] -> a
+average xs = sum xs / (fromIntegral $ length xs)
+
+-- XML utilities
+
+simpleName :: String -> QName
+simpleName name = QName name Nothing Nothing
+
+findAttr' :: String -> Element -> Maybe String
+findAttr' = findAttr . simpleName
+
+findElement' :: String -> Element -> Maybe Element
+findElement' = findElement . simpleName
+
+findElements' :: String -> Element -> [Element]
+findElements' = findElements . simpleName
+
+-- API access
+
+type Query = [(String, String)]
+type URL = String
+
+buildURL :: Query -> URL
+buildURL = ("http://weather.service.msn.com/find.aspx?" ++) . encodeQuery
+  where encodeQuery :: Query -> String
+        encodeQuery = intercalate "&" . map (\(a, b) -> a ++ "=" ++ b)
+
+type Location = String
+type Temperature = Double
+
+doRequest :: Location -> IO [Element]
+doRequest location =
+  (onlyElems . parseXML) <$> (getResponseBody =<< simpleHTTP request)
+  where url :: URL
+        url = buildURL [ ("src", "outlook")
+                       , ("culture", location)
+                       , ("weadegreetype", "C")
+                       , ("weasearchstr", location)
+                       ]
+        request :: Request String
+        request = getRequest url
+
+getTemperature :: Location -> IO Temperature
+getTemperature location = do
+  weatherData <- doRequest location
+  let locations = concatMap (findElements' "weather") weatherData
+      temperatures = mapMaybe getLocationTemperature locations
+  return $ average temperatures
+    where getLocationTemperature :: Element -> Maybe Temperature
+          getLocationTemperature location =
+            read <$> (findElement' "current" location >>= findAttr' "temperature")
+
+-- Main
+
+main' :: Location -> IO ()
+main' location = print =<< getTemperature location
+
+incorrectUsage :: String -> IO ()
+incorrectUsage message = do
+  hPutStrLn stderr message
+  exitWith (ExitFailure 1)
+
+main :: IO ()
+main = do
+  arguments <- getArgs
+  case arguments of
+    [] -> incorrectUsage "Place name should be passed as a command-line argument"
+    [location] -> main' location
+    _ -> incorrectUsage "Too many arguments"
---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    let
+      package = { haskell, stdenvNoCC }:
+        stdenvNoCC.mkDerivation {
+          name = "autofan";
+
+          src = ./.;
+          buildInputs = [
+            (haskell.packages.ghc901.ghcWithPackages
+              (haskellPackages: with haskellPackages; [ HTTP xml ]))
+          ];
+
+          buildPhase = "ghc -Wall Autofan.hs";
+          installPhase = "install -D Autofan $out/bin/autofan";
+        };
+
+      module = { pkgs, config, lib, ... }:
+        with lib;
+        let
+          cfg = config.services.autofan;
+          autofan = "${self.packages.${pkgs.system}.autofan}/bin/autofan";
+        in {
+          options.services.autofan = {
+            enable = mkEnableOption "Automatic fan controller";
+
+            location = mkOption {
+              description = "Location to fetch temperature data for.";
+              type = types.str;
+            };
+          };
+
+          config.systemd.services.autofan = {
+            description = "Automatic fan controller";
+
+            wants = [ "network-online.target" ];
+            after = [ "network-online.target" ];
+            wantedBy = [ "default.target" ];
+
+            serviceConfig = {
+              Restart = "on-failure";
+              ExecStart = "${autofan} ${escapeShellArg cfg.location}";
+            };
+
+            postStop = "echo 1-1 >/sys/bus/usb/drivers/usb/unbind || exit 0";
+          };
+        };
+
+    in {
+      nixosModules.autofan = module;
+    } // (utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ]
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          autofan = pkgs.callPackage package { };
+        in {
+          packages.autofan = autofan;
+          defaultPackage = autofan;
+        }));
+}
